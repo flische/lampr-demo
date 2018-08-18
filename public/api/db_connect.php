79b909428e5676214d987587f080a3be16278e8a
flische <flische@gmail.com>
2018-08-17 17:12:44 -0700

    toggle-complete

--- public/api/db_connect.php
+++ public/api/db_connect.php
@@ -6,8 +6,8 @@ $conn = mysqli_connect($host, $user, $password, $db_name);
 
 if(!$conn){
     $output['error'] = 'Error connecting to Database';
-
     print(json_encode($output));
-
     exit;
 }
+
+?>
